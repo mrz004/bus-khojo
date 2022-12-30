2e16a4881f216f82ae57a9b72bd3c46fcc6168d6
root <root@localhost.localdomain>
2022-12-30 14:08:04 +0530

    tried to fix map issue

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -1,20 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Map from "ol/Map.js";
 import View from "ol/View.js";
 import OSM from "ol/source/OSM.js";
 import TileLayer from "ol/layer/Tile.js";
 
 const MyMap = () => {
+
+  const loadMap = useRef(true);
+
   useEffect(() => {
-    console.log("useEffect was run!");
-    new Map({
-      layers: [new TileLayer({ source: new OSM() })],
-      view: new View({
-        center: [0, 0],
-        zoom: 2,
-      }),
-      target: "map",
-    });
+    if (loadMap.current) {
+      loadMap.current = false;
+      console.log("useEffect was run!");
+      new Map({
+        layers: [new TileLayer({ source: new OSM() })],
+        view: new View({
+          center: [0, 0],
+          zoom: 2,
+        }),
+        target: "map",
+      });
+    }
   }, []);
   return <div id="map" style={{ width: "90vh", height: "90vh" }}></div>;
 };
